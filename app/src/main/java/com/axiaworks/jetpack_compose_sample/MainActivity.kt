61ed6f1a8943c897f2b0330ffce9d1b56ce31902
t-nakata <toshihiro.nakata@axia-works.com>
2021-12-13 21:41:53 +0900

    Hello World!

--- app/src/main/java/com/axiaworks/jetpack_compose_sample/MainActivity.kt
+++ app/src/main/java/com/axiaworks/jetpack_compose_sample/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.axiaworks.jetpack_compose_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard("World")
+        }
     }
 }
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
+}
+
